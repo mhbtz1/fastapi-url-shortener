7f3f9ea170c398d3361ba8a7cc52f785539faf9e
Matthew Habtezgi <matt@habtem.dev>
2026-01-06 15:44:56 -0500

    raw client

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,11 @@
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins: [
+        viteReact({})
+    ],
+    server: {
+        port: 3000
+    }
+})
