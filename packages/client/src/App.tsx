2b239f36cc397f5005ecefda5ddcf547171ede3a
Matthew Habtezgi <matt@habtem.dev>
2026-01-06 18:33:53 -0500

    light client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,69 @@
+import { useMutation } from '@tanstack/react-query'
+import { useState, useEffect } from 'react'
+import { Toaster, toast } from 'sonner'
+
+interface Payload {
+    url: string
+}
+
 export const App = () => {
-    return (<>
-        Test
-    </>)
+    const [text, setText] = useState<string>('')
+    const [redirectState, setRedirectState] = useState<string>('')
+
+    const minifyMutation = useMutation<Payload, Error>({
+        mutationKey: ['minifyMutation'],
+        mutationFn: async () => {
+            const output = await fetch('http://127.0.0.1:8000/minify_url',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        url: text
+                    })
+                }
+            ).then( async (res) => res.json())
+            return output
+        }})
+
+    const redirectMutation = useMutation<void, Error>({
+        mutationKey: ['redirectMutation'],
+        mutationFn: async () => {
+            window.location.href = `http://127.0.0.1:8000/${redirectState}`
+        }
+    })
+
+    const minifyUrl = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const output = await minifyMutation.mutateAsync()
+        console.log(`output url: ${output.url}`)
+        return output
+    }
+
+    const redirectUrl = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        return await redirectMutation.mutate()
+    }
+
+    return (
+        <div>
+            <form id="min-form" onSubmit={async (e: React.FormEvent<HTMLFormElement>) => {
+                const output = await minifyUrl(e)
+                toast("Compressed URL created", {
+                    description: output.url,
+                    duration: 3000
+                })
+            }}>
+                <label id="url-one"> Minify URL </label> <br/>
+                <input id="url-one" type="text" onChange={(e) => setText(e.target.value)}/>
+                <button type="submit"> Submit </button>
+            </form>
+            <form id="rev-navigate" onSubmit={redirectUrl}>
+                <label id="url-two"> Redirect Form</label> <br/>
+                <input id="url-two" type="text" onChange={(e) => setRedirectState(e.target.value)}/>
+                <button type="submit"> Submit </button>
+            </form>
+        </div>
+    )
 }
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,14 @@
-import { createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner';
+
+const queryClient = new QueryClient()
 
 const root = document.getElementById('root')!
 
-createRoot(root).render(<App/>)
+createRoot(root).render(
+<QueryClientProvider client={queryClient}> 
+    <Toaster richColors/>
+     <App/> 
+</QueryClientProvider>)
